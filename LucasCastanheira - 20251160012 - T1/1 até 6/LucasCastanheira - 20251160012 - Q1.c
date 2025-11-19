8feffcf8f2aa908bfdfebbeb0a32269ad1f38ffa
Toshiyuki7266 <ltoshikc@gmail.com>
2025-11-19 13:16:16 -0300

    Update LucasCastanheira - 20251160012 - Q1.c

--- LucasCastanheira - 20251160012 - T1/1 até 6/LucasCastanheira - 20251160012 - Q1.c
+++ LucasCastanheira - 20251160012 - T1/1 até 6/LucasCastanheira - 20251160012 - Q1.c
@@ -3,69 +3,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-int transformar_data (char *data, int *dia, int *mes, int *ano){
-    printf ("Insira uma data válida no formato dd/mm/aaaa\n");
-    fgets (data, 11, stdin);
-    
-        if (sscanf(data, "%d/%d/%d", dia, mes, ano) == 3){
-            if (dia > 0 && mes > 0 && ano > 0){
-                switch (*mes){
-                    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                        if (*dia > 31){
-                            return 0;
-                        }
-                        else {
-                            return 1;
-                        }
-                    break;
-                    
-                    case 4: case 6: case 9: case 11:
-                        if (*dia > 30){
-                            return 0;
-                        }
-                        else {
-                            return 1;
-                        }
-                    break;
-                    
-                    case 2:
-                        if (*ano % 4 == 0 && *ano % 100 != 0 || *ano % 400 == 0){
-                            if (*dia > 29){
-                                return 0;
-                            }
-                            
-                            else {
-                                return 1;
-                            }
-                        }
-                        else{
-                            if (*dia > 28){
-                                return 0;
-                            }
-                            
-                            else {
-                                return 1;
-                            }
-                        }
-                    break;
-                    
-                    default:
-                        return 0;
-                }
-                
-            }
-            return 1;
-        }
-        else {
+int ehBissexto(int ano) {
+    return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+}
+
+int validar_data(char *data, int *dia, int *mes, int *ano) {
+    printf("Insira uma data válida no formato dd/mm/aaaa\n");
+    fgets(data, 11, stdin);
+
+    if (sscanf(data, "%d/%d/%d", dia, mes, ano) != 3)
+        return 0;
+
+    if (*dia <= 0 || *mes <= 0 || *ano <= 0)
+        return 0;
+
+    switch (*mes) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            return (*dia <= 31);
+
+        case 4: case 6: case 9: case 11:
+            return (*dia <= 30);
+
+        case 2:
+            if (ehBissexto(*ano))
+                return (*dia <= 29);
+            else
+                return (*dia <= 28);
+
+        default:
             return 0;
-        }
+    }
 }
 
 int main() {
     char data[11];
     int dia, mes, ano;
 
-    if (transformar_data(data, &dia, &mes, &ano)) {
+    if (validar_data(data, &dia, &mes, &ano)) {
         printf("Dia: %d\nMes: %d\nAno: %d\n", dia, mes, ano);
         printf("Data válida");
     } else {
