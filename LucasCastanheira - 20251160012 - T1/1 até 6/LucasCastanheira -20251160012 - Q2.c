167cb202fa38c8a80e85fef47a1e6567b465e834
Toshiyuki7266 <ltoshikc@gmail.com>
2025-11-19 13:50:33 -0300

    Update LucasCastanheira -20251160012 - Q2.c

--- LucasCastanheira - 20251160012 - T1/1 até 6/LucasCastanheira -20251160012 - Q2.c
+++ LucasCastanheira - 20251160012 - T1/1 até 6/LucasCastanheira -20251160012 - Q2.c
@@ -1,92 +1,90 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int validar_data (char *data, int *dia, int *mes, int *ano){
-    printf ("Insira a data do seu aniversario no formato dd/mm/aaaa\n");
-    fgets (data, 11, stdin);
-    
-        if (sscanf(data, "%d/%d/%d", dia, mes, ano) == 3){
-            if (dia > 0 && mes > 0 && ano > 0){
-                switch (*mes){
-                    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                        if (*dia > 31){
-                            return 0;
-                        }
-                        else {
-                            return 1;
-                        }
-                    break;
-                    
-                    case 4: case 6: case 9: case 11:
-                        if (*dia > 30){
-                            return 0;
-                        }
-                        else {
-                            return 1;
-                        }
-                    break;
-                    
-                    case 2:
-                        if (*ano % 4 == 0 && *ano % 100 != 0 || *ano % 400 == 0){
-                            if (*dia > 29){
-                                return 0;
-                            }
-                            
-                            else {
-                                return 1;
-                            }
-                        }
-                        else{
-                            if (*dia > 28){
-                                return 0;
-                            }
-                            
-                            else {
-                                return 1;
-                            }
-                        }
-                    break;
-                    
-                    default:
-                        return 0;
-                }
-                
-            }
-            return 1;
-        }
-        else {
-            return 0;
-        }
+int ehBissexto(int ano) {
+    return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
 }
 
-int verificar_tempo (char *data2, int *dia2, int *mes2, int *ano2){
-    if (validar_data(data, &dia, &mes, &ano)){
-        printf("Insira a data que deseja verificar a distancia");
-        fgets (data2, 11, stdin);
-        
-        if (sscanf(data2, "%d/%d/%d", dia2, mes2, ano2) == 3){
-            validar_data(data2, &)
-        };
-        
+int diasDoMes(int mes, int ano) {
+    switch (mes) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            return 31;
+        case 4: case 6: case 9: case 11:
+            return 30;
+        case 2:
+            return ehBissexto(ano) ? 29: 28;
+        default:
+            return 0;
     }
-    
-    else {
+}
+
+int validar_data(char *data, int *dia, int *mes, int *ano) {
+    fgets(data, 22, stdin);
+
+    if (sscanf(data, "%d/%d/%d", dia, mes, ano) != 3){
         return 0;
     }
+    else if (*dia <= 0 || *mes <= 0 || *ano <= 0){
+        return 0;
+    }
+    else if (*dia > diasDoMes(*mes, *ano)){
+        return 0;
+    }
+    
+    else{
+        return 1;
+    }
+    
+}
+
+void diferencaDatas(int d1, int m1, int a1,
+                    int d2, int m2, int a2,
+                    int *anos, int *meses, int *dias) {
+
+    if (d2 < d1) {
+        m2--;
+        if (m2 == 0) {
+            m2 = 12;
+            a2--;
+        }
+        d2 += diasDoMes(m2, a2);
+    }
+
+    if (m2 < m1) {
+        m2 += 12;
+        a2--;
+    }
+
+    *dias  = d2 - d1;
+    *meses = m2 - m1;
+    *anos  = a2 - a1;
 }
 
 int main() {
-    char data[11];
-    int dia, mes, ano;
+    char data[22];
+    int d1, m1, a1;
+    int d2, m2, a2;
+    int anos, meses, dias;
 
-    if (validar_data(data, &dia, &mes, &ano)) {
-        printf("Dia: %d\nMes: %d\nAno: %d\n", dia, mes, ano);
-        printf("Data válida");
-    } else {
-        printf("Formato inválido!\n");
+    printf("Insira a primeira data (dd/mm/aaaa):\n");
+    if (!validar_data(data, &d1, &m1, &a1)) {
+        printf("Data inválida!\n");
+        return 0;
     }
+
+    printf("Insira a segunda data (dd/mm/aaaa):\n");
+    if (!validar_data(data, &d2, &m2, &a2)) {
+        fflush(stdin);
+        printf("Data inválida!\n");
+        return 0;
+    }
+
+    diferencaDatas(d1, m1, a1, d2, m2, a2, &anos, &meses, &dias);
+    
+
+    printf("\nDiferença entre as datas:\n");
+    printf("%d anos, %d meses e %d dias\n", anos, meses, dias);
 
     return 0;
 }
