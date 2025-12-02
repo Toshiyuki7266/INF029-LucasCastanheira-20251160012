6bb939b75ee5a50cb1712d5b5cdc41e6f05935ce
Toshiyuki7266 <ltoshikc@gmail.com>
2025-12-02 17:54:30 -0300

    Update LucasCastanheira20251160012.c

--- LucasCastanheira - 20251160012 - T1/TRABALHO1 CORRIGIDO E NO FORMATO IDEAL/LucasCastanheira20251160012.c
+++ LucasCastanheira - 20251160012 - T1/TRABALHO1 CORRIGIDO E NO FORMATO IDEAL/LucasCastanheira20251160012.c
@@ -22,7 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "LucasCastanheira20251160012.h"
+#include "trabalho1.h"
 
 int q1(char data[])
 {
@@ -260,5 +260,6 @@ int q7(char matriz[8][10], char busca[])
     return 0;
 
 }
+
 
 
